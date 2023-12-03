01a5ca0d8f3b9d64aef1b9ac4d3cd5d15cefe92a
LeeKung <abdulloh.eg@gmail.com>
2023-12-03 09:21:15 +0700

    Delete lab-CreateVM/provider.tf

--- lab-CreateVM/provider.tf
+++
@@ -1,9 +0,0 @@
-#Add Provider
-provider "azurerm" {
-  subscription_id = "968f8832-5205-42de-949e-9055216e4457"
-  client_id       = "ac2f3d26-985c-4234-a9e4-74a4b1ed2d12"
-  client_secret   = "3Ml8Q~R35-C8F66_oaYrygeUxD564_GRny8hjcdE"
-  tenant_id       = "8a0ccc76-6ff8-4b9f-85eb-6287228a9e07"
-  features {}
-}
-
